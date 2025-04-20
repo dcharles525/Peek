8b32740ab2b010df248e58712597c6366918979e
David Johnson <dcharles525@gmail.com>
2025-04-20 19:43:17 -0400

    feat: cleaned up app and wrote tests

--- main.go
+++ main.go
@@ -2,71 +2,42 @@ package main
 
 import (
 	"fmt"
-  "sort"
 	"math/rand"
 	"os"
 	"strconv"
-	"sync"
 )
 
 const charSet = `abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 ,.!?`
 const seed = 1
 const bookLength = 32000
 
-func generateRandomString(wg *sync.WaitGroup, f int, books chan<- map[int]string) {
-	defer wg.Done()
-  randSeeded := rand.New(rand.NewSource(int64(f)))
-	b := make([]byte, bookLength + 100)
-  for i := 0; i < bookLength; i++ {
-		b[i] = charSet[randSeeded.Intn(len(charSet))]
+func generateRandomString(bookPage int) []byte {
+	randSeeded := rand.New(rand.NewSource(int64(bookPage)))
+	book := make([]byte, bookLength)
+
+	for i := 0; i < bookLength; i++ {
+		book[i] = charSet[randSeeded.Intn(len(charSet))]
 	}
 
-	book := make(map[int]string)
-	book[f] = string(b)
-	books <- book
+	return book
 }
 
-func seekBook(bookNumber int) string {
-	var wg sync.WaitGroup
-	books := make(chan map[int]string, bookNumber)
-
-	go func() {
-		wg.Wait()
-		close(books)
-	}()
-
-	for i := 0; i < bookNumber; i++ {
-		wg.Add(1)
-		go generateRandomString(&wg, i, books)
-	}
-
-	finalResults := make(map[int]string)
-	for book := range books {
-		for k, v := range book {
-			finalResults[k] = v
-		}
-	}
-
-  keys := make([]int, 0, len(finalResults))
-	for k := range finalResults {
-		keys = append(keys, k)
-	}
-  sort.Ints(keys)
-
-	return finalResults[keys[len(keys)-1]]
-}
-
-func main() {
-	if len(os.Args) != 2 {
+func seekBook(args []string) []byte {
+	if len(args) != 2 {
 		fmt.Println("Usage: go run main.go <book>")
-		return
+		return []byte{}
 	}
 
-	numBooks, numBooksError := strconv.Atoi(os.Args[1])
+	numBooks, numBooksError := strconv.Atoi(args[1])
 
 	if numBooksError != nil {
 		fmt.Println("Usage: Enter a valid integer value for the book number")
+		return []byte{}
 	}
 
-	fmt.Println(seekBook(numBooks))
+	return generateRandomString(numBooks)
+}
+
+func main() {
+	fmt.Println(string(seekBook(os.Args)))
 }
